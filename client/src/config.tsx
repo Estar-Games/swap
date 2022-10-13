0f9caef4727810e71703bd16ed5dcded63501b1e
EdiMarian <ichimedimarian@gmail.com>
2022-10-13 23:33:01 +0300

    .

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,7 +1,7 @@
 export const contractAddress =
   'erd1qqqqqqqqqqqqqpgqrrccfuyx3hmdpyg330cnf8r93qc8g67zwmfs7l8qsq';
 export const token = 'ESTAR-afaaf0';
-export const microservice = 'http://equistar.estar.games:1212';
+export const microservice = 'https://equistar.estar.games:1212';
 export const newTokenIdentifier = 'ESTAR-461bab';
 
 export const dAppName = 'Estar Games';
