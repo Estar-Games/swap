53577f6ea9f7ef0f876737b0278bc9c71e5d7c5b
EdiMarian <ichimedimarian@gmail.com>
2022-10-13 23:31:08 +0300

    .

--- micro-service/dist/main.js
+++ micro-service/dist/main.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
+const httpsOptions = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+};
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true, httpsOptions });
     await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
--- micro-service/src/main.ts
+++ micro-service/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
